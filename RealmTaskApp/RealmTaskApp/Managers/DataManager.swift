2aa7978db7ddb74f277d69dceef9183807164c77
Val <82764150+hopkee@users.noreply.github.com>
2022-03-12 18:09:29 +0300

    Commit two

---
+++ RealmTaskApp/RealmTaskApp/Managers/DataManager.swift
@@ -0,0 +1,63 @@
+//
+//  DataManager.swift
+//  RealmTaskApp
+//
+//  Created by Valya on 12.03.22.
+//
+
+import Foundation
+import RealmSwift
+
+class DataManager {
+    
+    static func addNewTask(_ newTask: Task) {
+        
+        let realm = try! Realm()
+        
+        do {
+            try realm.write({
+                realm.add(newTask)
+            })
+        } catch let Error {
+            print("Error in writing data: \(debugPrint(Error))")
+        }
+        
+    }
+    
+    static func getAllTasks() -> [Task] {
+        
+        let realm = try! Realm()
+        let allTasks = realm.objects(Task.self).toArray(ofType: Task.self) as [Task]
+        
+        return allTasks
+    }
+    
+    static func deleteTask(_ taskToDelete: Task) {
+        
+        let realm = try! Realm()
+        
+        do {
+            try realm.write({
+                realm.delete(taskToDelete)
+            })
+        } catch let Error {
+            print("Error in writing data: \(debugPrint(Error))")
+        }
+        
+    }
+    
+    static func deleteAllTasks() {
+        
+        let realm = try! Realm()
+        
+        do {
+            try realm.write({
+                realm.deleteAll()
+            })
+        } catch let Error {
+            print("Error in writing data: \(debugPrint(Error))")
+        }
+        
+    }
+    
+}
---
+++ RealmTaskApp/RealmTaskApp/Model/Model.swift
@@ -0,0 +1,28 @@
+//
+//  Model.swift
+//  RealmTaskApp
+//
+//  Created by Valya on 12.03.22.
+//
+
+import Foundation
+import RealmSwift
+
+class Task: Object {
+    @Persisted var title: String?
+    @Persisted var detailText: String?
+    @Persisted var date: Date?
+}
+
+extension Results {
+    func toArray<T>(ofType: T.Type) -> [T] {
+        var array = [T]()
+        for i in 0 ..< count {
+            if let result = self[i] as? T {
+                array.append(result)
+            }
+        }
+
+        return array
+    }
+}
